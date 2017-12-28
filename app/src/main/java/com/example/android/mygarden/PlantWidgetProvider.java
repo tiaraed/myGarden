73d4dd36d116bd1d2ca0dbf3dc0452e34cd427f2
Mokleters <tiara.eka99@gmail.com>
2017-12-28 21:28:28 +0700

    Add Watering Service

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -18,7 +18,6 @@ public class PlantWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        // Create an Intent to launch MainActivity when clicked
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         // Construct the RemoteViews object
@@ -26,6 +25,10 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         // Widgets allow click handlers to only launch pending intents
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
         // Instruct the widget manager to update the widget
+        Intent wateringIntent = new Intent(context, PlantWateringService.class);
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0, wateringIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.widget_water_button, wateringPendingIntent);
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
